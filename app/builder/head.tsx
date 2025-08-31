49d09348239f7b2d068864f4b5a7c71d640ce84a
lironvaknin-beep <lironvaknin@gmail.com>
2025-08-31 22:09:08 +0300

    Update head.tsx

--- app/builder/head.tsx
+++ app/builder/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <link id="builder-css" rel="stylesheet" href="/builder/style.css" />
+      <link id="builder-css" rel="stylesheet" href="/builder/style.css?V=1" />
     </>
   );
 }
