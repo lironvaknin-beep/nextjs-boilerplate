c115d89bdf8e99802ba45079536905c517c6c008
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-07 15:07:49 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,8 @@
 import createMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale } from './i18n';
  
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: locales,
- 
-  // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: defaultLocale
-});
+export default createMiddleware({ locales, defaultLocale, localeDetection: true });
  
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips
-  // certain folders and all pathnames with a dot (e.g. favicon.ico)
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!_next|.*\\..*).*)']
 };
