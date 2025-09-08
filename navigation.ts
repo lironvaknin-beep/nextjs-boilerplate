693bced474d3992161ced3ae80dd3231a2d424ec
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-08 20:25:27 +0300

    Delete navigation.ts

--- navigation.ts
+++
@@ -1,9 +0,0 @@
-import { locales } from './i18n';
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-
-// מכבד את ההגדרה במידלוור שה־default אין לו קידומת
-export const { Link, usePathname, useRouter, redirect } =
-  createSharedPathnamesNavigation({
-    locales,
-    localePrefix: 'as-needed'
-  });
