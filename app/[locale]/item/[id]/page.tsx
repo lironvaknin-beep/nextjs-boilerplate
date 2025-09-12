236142065749438b6bd2de8e8a13c533a9823294
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-12 15:44:14 +0300

    Update page.tsx

--- app/[locale]/item/[id]/page.tsx
+++ app/[locale]/item/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Link } from '@/navigation';
-import { useParams } from 'next/navigation';
+import {Link} from '@/navigation';
+import {useParams} from 'next/navigation';
 import styles from './item.module.css';
 import sampleData from '@/app/sample-data.json';
 
