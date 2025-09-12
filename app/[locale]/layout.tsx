96ba0be166962cf464f50aff6739c6bec3fd8cbd
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-12 15:08:44 +0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,20 +1,28 @@
-import { NextIntlClientProvider, AbstractIntlMessages } from 'next-intl';
-import { notFound } from 'next/navigation';
-import type { ReactNode } from 'react';
-import { locales } from '../../i18n';
-
 import '../globals.css';
+
+import type { ReactNode } from 'react';
+import { NextIntlClientProvider } from 'next-intl';
+import { notFound } from 'next/navigation';
+
+import { locales } from '../../i18n';
 import Header from '../(ui)/Header';
 import AppFooter from '../(ui)/AppFooter';
 import PreferencesProvider from '../(ui)/PreferencesProvider';
 
-type Props = { children: ReactNode; params: { locale: string } };
-
-export default async function LocaleLayout({ children, params }: Props) {
+export default async function LocaleLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { locale: string };
+}) {
   const { locale } = params;
-  if (!locales.includes(locale as any)) notFound();
 
-  const messages = (await import(`../../messages/${locale}.json`)).default as AbstractIntlMessages;
+  if (!locales.includes(locale as any)) {
+    notFound();
+  }
+
+  const messages = (await import(`../../messages/${locale}.json`)).default;
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
