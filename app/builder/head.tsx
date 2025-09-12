ff58fa610fc5e21b0ebe2be44a79fe1564d67ad1
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-12 16:08:49 +0300

    Delete app/builder directory

--- app/builder/head.tsx
+++
@@ -1,7 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <link id="builder-css" rel="stylesheet" href="/builder/style.css?V=1" />
-    </>
-  );
-}
--- app/builder/layout.tsx
+++
@@ -1,5 +0,0 @@
-import type { ReactNode } from 'react';
-
-export default function BuilderLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
-}
