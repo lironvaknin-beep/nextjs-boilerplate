eb8a0bddd0c07900282e562bcdf9a99a7f844429
lironvaknin-beep <lironvaknin@gmail.com>
2025-09-12 14:48:44 +0300

    Create navigation.ts

---
+++ navigation.ts
@@ -0,0 +1,8 @@
+import { locales } from './i18n';
+import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+
+export const { Link, usePathname, useRouter, redirect } =
+  createSharedPathnamesNavigation({
+    locales,
+    localePrefix: 'as-needed' // EN בלי קידומת, אחרות עם קידומת
+  });
